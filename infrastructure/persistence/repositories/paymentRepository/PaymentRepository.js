ef8cfd4b8cae51b62fc393cdc598a1421c882e6d
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2024-11-24 16:08:18 -0300

    refactor: PaymentRepository

---
+++ infrastructure/persistence/repositories/paymentRepository/PaymentRepository.js
@@ -0,0 +1,23 @@
+const IPaymentRepository = require('../../../../domain/useCases/IPaymentRepository');
+
+class PaymentRepository extends IPaymentRepository {
+    constructor(db) {
+        super();
+        if (!db) {
+            throw new Error('A instância do banco de dados é obrigatória.');
+        }
+        this.collection = db.collection('paymentsCollection');
+    }
+
+    async save(order) {
+        try {
+            const result = await this.collection.insertOne(order);
+            console.log(`Pedido salvo no Banco de pagamento com ID: ${result.insertedId}`);
+        } catch (err) {
+            console.error('Erro ao salvar pagamento no Banco:', err);
+            throw err;
+        }
+    }
+}
+
+module.exports = PaymentRepository;
